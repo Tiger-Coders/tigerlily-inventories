5b5796aea1caeaa76b875ef264b6bfda5a41790f
ZAF07 <zaffere07@gmail.com>
2022-11-04 23:09:46 +0800

    moved main.go to root. changed start-server script accordingly

--- start-server.sh
+++ start-server.sh
@@ -1,1 +1,3 @@
-go run cmd/main.go DEV localhost zaffere "" tigerlily disable "5432"
+# go run cmd/main.go DEV localhost zaffere "" tigerlily disable "5432"
+
+go run main.go DEV localhost zaffere "" tigerlily disable "5432"
